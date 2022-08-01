1a9b48ca96d7fa169b9afa3a60660684155be061
snsyzf <snsyzf@qq.com>
2022-08-01 09:35:36 +0800

    chore: support 2022.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,12 @@ buildscript {
 }
 
 plugins {
-    id("org.jetbrains.intellij") version "1.5.3"
+    id("org.jetbrains.intellij") version "1.7.0"
     kotlin("jvm") version "1.6.20"
 }
 
 group = "com.gitee.plugins"
-version = "1.0.9"
+version = "1.0.10"
 
 repositories {
     maven("https://maven.aliyun.com/repository/central")
@@ -26,7 +26,7 @@ dependencies {
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    this.version.set("2022.1")
+    this.version.set("2022.2")
 }
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
@@ -34,5 +34,5 @@ tasks.getByName<Test>("test") {
 tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
     this.version.set(project.version.toString())
     this.sinceBuild.set("193.*")
-    this.untilBuild.set("221.*")
+    this.untilBuild.set("222.*")
 }
